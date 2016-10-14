52489a69d4a447667022bbd9ba337460636d2a55
Jelle Spijker <spijker.jelle@gmail.com>
2016-10-14 17:36:07 +0200

    Initial object created

---
+++ ohCaptain/include/Sensors/Razor.h
@@ -0,0 +1,16 @@
+//
+// Created by peer23peer on 10/14/16.
+//
+
+#ifndef OHCAPTAIN_MAIN_RAZOR_H
+#define OHCAPTAIN_MAIN_RAZOR_H
+
+
+
+class Razor {
+
+};
+
+
+
+#endif //OHCAPTAIN_MAIN_RAZOR_H
---
+++ ohCaptain/src/Sensors/Razor.cpp
@@ -0,0 +1,5 @@
+//
+// Created by peer23peer on 10/14/16.
+//
+
+#include "Razor.h"
